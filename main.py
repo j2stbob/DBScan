4c8a96d2340afbcc67efbf8ecf993e749bbceb90
j2stbob <rotan.bohdan@gmail.com>
2025-01-27 15:17:07 +0300

    add main file

---
+++ main.py
@@ -0,0 +1,22 @@
+import DBScan as db
+import pandas as pd
+import numpy as np
+
+
+data = pd.read_csv("../Бауманка/Datas/color.csv")
+data = data.drop(columns=["color"])
+data_array = np.array(data)
+data = []
+for i in data_array:
+    data.append(list(i))
+
+
+def main():
+    dbscan = db.DBSCAN(5, 25, data)
+    cluster = dbscan.clustering()
+    dbscan.draw(cluster)
+
+
+if __name__ == "__main__":
+    main()
+
